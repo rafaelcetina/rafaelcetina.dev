3fa89f983323f7d1c381010332a6acab92f4482b
rdzul <rdzul@github.palaceresorts.com>
2024-03-05 14:55:29 -0600

    fix: :bug: fixed dark mode icon

--- src/components/icons/BarsIcon.vue
+++ src/components/icons/BarsIcon.vue
@@ -7,8 +7,9 @@ const props = defineProps({
   }
 })
 </script>
+
 <template>
-  <svg fill="#000000" :width="size" :height="size" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <svg fill="currentColor" :width="size" :height="size" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M3,8H21a1,1,0,0,0,0-2H3A1,1,0,0,0,3,8Zm18,8H3a1,1,0,0,0,0,2H21a1,1,0,0,0,0-2Zm0-5H3a1,1,0,0,0,0,2H21a1,1,0,0,0,0-2Z" />
   </svg>
